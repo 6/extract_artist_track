cc690fdb3ec0c46c5ec105fef5360598f59a7685
Peter Graham <peterghm@gmail.com>
2016-09-30 01:19:46 -0400

    test nil and empty cases

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,4 +1,18 @@
 describe YoutubeArtistTrackParser::Parser do
+  context "with nil title" do
+    subject { described_class.new(title: nil) }
+
+    its(:artist) { is_expected.to be_nil }
+    its(:track) { is_expected.to be_nil }
+  end
+
+  context "with empty title" do
+    subject { described_class.new(title: "") }
+
+    its(:artist) { is_expected.to be_nil }
+    its(:track) { is_expected.to be_nil }
+  end
+
   YAML.load_file('./spec/fixtures/songs.yml').each do |song|
     describe song["title"] do
       subject { described_class.new(title: song["title"]) }
