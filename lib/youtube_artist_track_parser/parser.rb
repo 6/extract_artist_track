e66ad5b8f8a6e91be565a844a4b3ad41d5d77921
Peter Graham <peterghm@gmail.com>
2016-09-30 01:17:43 -0400

    simplify code

--- lib/youtube_artist_track_parser/parser.rb
+++ lib/youtube_artist_track_parser/parser.rb
@@ -1,35 +1,29 @@
 module YoutubeArtistTrackParser
   class Parser
-    attr_reader :title, :splitter
     def initialize(title:)
       @title = Cleaner.clean_title(title)
-      @splitter = Splitter.new(title: @title)
     end
 
     def artist
       return @artist if defined?(@artist)
-      return @artist = nil if invalid? || split_artist.nil?
-      @artist = Cleaner.clean_artist(split_artist)
+      return @artist = nil if invalid? || splitter.artist.nil?
+      @artist = Cleaner.clean_artist(splitter.artist)
     end
 
     def track
       return @track if defined?(@track)
-      return @track = nil if invalid? || split_track.nil?
-      @track = Cleaner.clean_track(split_track)
+      return @track = nil if invalid? || splitter.track.nil?
+      @track = Cleaner.clean_track(splitter.track)
     end
 
   private
 
     def invalid?
-      title.nil? || title.empty?
+      @title.nil? || @title.empty?
     end
 
-    def split_artist
-      @split_artist ||= splitter.artist
-    end
-
-    def split_track
-      @split_artist ||= splitter.track
+    def splitter
+      @splitter ||= Splitter.new(title: @title)
     end
   end
 end
